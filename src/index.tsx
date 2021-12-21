3d08b44835db82942ba3f13ff4aa9f4e6c7bb702
MetalTurtle <dekolis1218@gmail.com>
2021-12-21 15:26:36 -0500

    Replace class components with functional components

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, ComponentProps, ComponentState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -9,74 +9,54 @@ import './index.css';
 //  [ ] maybe animations
 //  [ ] maybe persistence (localstorage works fine)
 
-class AddItemsPane extends React.Component<ComponentProps<any>, ComponentState> {
-    constructor(props: { onButtonClick: (text: String) => void }) {
-        super(props);
-        this.state = {
-            text: '',
-        };
+const AddItemsPane = (props: { onButtonClick: (text: String) => void }) => {
+    const [text, setText] = useState('');
+
+    return (
+        <div>
+            <button onClick={() => {
+                props.onButtonClick(text);
+                setText('');
+            }}>
+                Add item
+            </button>
+            <input type="text"
+                   placeholder="Enter item"
+                   value={text}
+                   onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                       setText(event.target.value)
+                   }
+            />
+        </div>
+    )
+};
+
+const App = () => {
+    const [items, setItems] = useState(Array<String>());
+
+    const addItem = (text: String) => {
+        setItems(items.concat(text));
     }
 
-    render() {
-        return (
+    return (
+        <>
+            <AddItemsPane onButtonClick={addItem}/>
             <div>
-                <button onClick={() => {
-                    this.props.onButtonClick(this.state.text)
-                    this.setState({text: ''})
-                }}>
-                    Add item
-                </button>
-                <input type="text"
-                       placeholder="Enter item"
-                       value={this.state.text}
-                       onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                           this.setState({text: event.target.value})
-                       }
-                />
+                <ul>
+                    {items.map(
+                        (item: String, index: Number) => <li key={index.toString()}>{item}</li>
+                    )}
+                </ul>
             </div>
-        )
-    }
-}
-
-class App extends React.Component<ComponentProps<any>, ComponentState> {
-    constructor(props: ComponentProps<any>) {
-        super(props);
-        this.state = {
-            items: Array<String>(),
-        };
-    }
-
-    addItem = (text: String) => {
-        this.setState({
-            items: this.state.items.concat(text),
-        });
-    }
-
-    render() {
-        return (
-            <>
-                <AddItemsPane onButtonClick={this.addItem}/>
-                <div>
-                    <ul>
-                        {this.state.items.map(
-                            (item: String, index: Number) => <li key={index.toString()}>{item}</li>
-                        )}
-                    </ul>
-                </div>
-            </>
-        )
-    }
+        </>
+    )
 }
 
 // ========================================
 
-ReactDOM
-    .render(
-        <React.StrictMode>
-            <App/>
-        </React.StrictMode>,
-        document
-            .getElementById(
-                'root'
-            )
-    );
+ReactDOM.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
