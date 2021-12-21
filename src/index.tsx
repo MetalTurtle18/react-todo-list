141fc691c7fd3bbf5b0b32972ec0625dcee797ce
MetalTurtle <dekolis1218@gmail.com>
2021-12-21 16:54:42 -0500

    Complete second todo list goal with minor style issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 // TODO:
 //  [x] add and remove items
-//  [ ] toggle items from being done to undone
+//  [x] toggle items from being done to undone
 //  [ ] filter out done items
 //  [ ] maybe animations
 //  [ ] maybe persistence (localstorage works fine)
@@ -23,13 +23,28 @@ const AddItemsPane = (props: { onButtonClick: (text: String) => void }) => {
             <input type="text"
                    placeholder="Enter item"
                    value={text}
-                   onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                   onChange={(event) =>
                        setText(event.target.value)
                    }
             />
         </div>
     )
 };
+
+const Item = (props: { text: String, key: React.Key }) => {
+    const [done, setDone] = useState(false);
+
+    return (
+        <div className={"TodoItem" + (done ? " done" : "")}>
+            <input type="checkbox"
+                   onChange={(event) =>
+                       setDone(event.target.checked)
+                   }
+            />
+            <li key={props.key}>{props.text}</li>
+        </div>
+    )
+}
 
 const App = () => {
     const [items, setItems] = useState(Array<String>());
@@ -39,16 +54,14 @@ const App = () => {
     }
 
     return (
-        <>
+        <div>
             <AddItemsPane onButtonClick={addItem}/>
             <div>
                 <ul>
-                    {items.map(
-                        (item: String, index: Number) => <li key={index.toString()}>{item}</li>
-                    )}
+                    {items.map((item, key) => <Item key={key} text={item}/>)}
                 </ul>
             </div>
-        </>
+        </div>
     )
 }
 
