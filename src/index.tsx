7512dec2f65628d266e3f558aa098e4c7356d109
MetalTurtle <dekolis1218@gmail.com>
2021-12-27 12:51:21 -0800

    First attempt at filtering using bad indexing method

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -9,13 +9,13 @@ import './index.css';
 //  [ ] maybe animations
 //  [ ] maybe persistence (localstorage works fine)
 
-const AddItemsPane = (props: { onButtonClick: (text: String) => void }) => {
+const ControlPanel = (props: { onAddButtonClick: (text: string) => void, onFilterButtonClick: () => void }) => {
     const [text, setText] = useState('');
 
     return (
         <div>
             <button onClick={() => {
-                props.onButtonClick(text);
+                props.onAddButtonClick(text);
                 setText('');
             }}>
                 Add item
@@ -27,38 +27,64 @@ const AddItemsPane = (props: { onButtonClick: (text: String) => void }) => {
                        setText(event.target.value)
                    }
             />
+            <button onClick={props.onFilterButtonClick}>
+                Show Incomplete
+            </button>
         </div>
     )
 };
 
-const Item = (props: { text: String, key: React.Key }) => {
+const Item = (props: { text: string, key: React.Key, onChange: (event: ChangeEvent<HTMLInputElement>) => void }) => {
+    // const [text, setText] = useState(props.text);
+    // const [checked, setChecked] = useState(false);
     const [done, setDone] = useState(false);
 
     return (
-        <div className={"TodoItem" + (done ? " done" : "")}>
-            <input type="checkbox"
-                   onChange={(event) =>
-                       setDone(event.target.checked)
-                   }
-            />
-            <li key={props.key}>{props.text}</li>
-        </div>
+        <li key={props.key}>
+            <div className={done ? "todo-item-done" : "todo-item"}>
+                <input type="checkbox"
+                       onChange={(event) => {
+                           setDone(event.target.checked)
+                           props.onChange(event)
+                       }}
+                />
+                <p className="todo-item-text">{props.text}</p>
+            </div>
+        </li>
     )
 }
 
 const App = () => {
-    const [items, setItems] = useState(Array<String>());
+    const [items, setItems] = useState(Array<{ text: string, completed: boolean }>());
+    const [isFiltered, setIsFiltered] = useState(false);
 
-    const addItem = (text: String) => {
-        setItems(items.concat(text));
+    const addItem = (text: string) => {
+        setItems(items.concat({text: text, completed: false}));
     }
+
+    const boxChanged = (checked: boolean, index: number) => {
+        const newItem = items[index];
+        newItem.completed = checked;
+        setItems(items.slice(0, index).concat(newItem).concat(items.slice(index + 1)));
+    }
+
+    const displayItems = () => items.filter((item) => !(item.completed && isFiltered))
 
     return (
         <div>
-            <AddItemsPane onButtonClick={addItem}/>
+            <ControlPanel onAddButtonClick={addItem}
+                          onFilterButtonClick={() =>
+                              setIsFiltered(!isFiltered)
+                          }
+            />
             <div>
                 <ul>
-                    {items.map((item, key) => <Item key={key} text={item}/>)}
+                    {displayItems().map((item, key) =>
+                        <Item text={item.text}
+                              key={key}
+                              onChange={(event) => boxChanged(event.target.checked, key)}
+                        />
+                    )}
                 </ul>
             </div>
         </div>
